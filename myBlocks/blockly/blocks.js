e54d81b03858d5fe49e51ce10512cc25cb76f289
sheniyuki <74400692+sheniyuki@users.noreply.github.com>
2020-11-13 16:01:33 +0800

    Add files via upload

---
+++ myBlocks/blockly/blocks.js
@@ -0,0 +1,10 @@
+Blockly.Blocks['sayhello'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("打招呼");
+    this.setOutput(true, null);
+    this.setColour(245);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ myBlocks/blockly/javascript.js
@@ -0,0 +1,6 @@
+Blockly.JavaScript['sayhello'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert("你好")';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
